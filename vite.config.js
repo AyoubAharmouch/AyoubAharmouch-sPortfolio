582a503ba4fd4f78785fcd1788394bf843b9aa36
AyoubAharmouch <ayoub.aharmouch305@gmail.com>
2025-01-17 23:17:24 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AyoubAharmouch-sPortfolio/', // Adjust this if your repo name is differentnpm run build
+  base: '/AyoubAharmouch-sPortfolio/' // Adjust this if your repo name is differentnpm run build
 
 })
 
